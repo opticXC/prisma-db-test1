fc4360f8f0ed32c5645b0d5678b3a321c5549b42
opticXC <aabishmalik018@gmail.com>
2023-10-24 16:04:15 +0530

    more docker updates

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,15 @@ FROM oven/bun:latest
 
 WORKDIR /app
 
-COPY package*.json ./
+COPY package*.json ./package.json
+COPY bun.lockb ./bun.lockb
+COPY sources ./sources
+COPY prisma ./prisma
+
 RUN bun install
-RUN bun prisma generate
+RUN bun x prisma generate
 COPY . .
 
+EXPOSE 8080
 
-CMD [ "bun", "sources/index.ts" ]
+CMD [ "bun", "run", "sources/index.ts" ]
--- sources/commons.ts
+++ sources/commons.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+
+import { PrismaClient } from '@prisma/client/edge';
 import dotenv from 'dotenv'
 dotenv.config();
 
-export const port = process.env.PORT || 8000;
+export const port = process.env.PORT || 8080;
 export const public_url = process.env.PUBLIC_URL || "http://localhost"
 
 export const prisma = new PrismaClient();
